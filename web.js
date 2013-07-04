f6b3fc124b6628bcda9eaf5278cd5c267070f1a8
Khaleel <kaleel@user.com>
2013-07-04 15:45:58 -0500

    fixed app crash

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 
+var options = {
+	encoding: 'utf8'
+};
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(fs.readFileSync('index.html', options));
 });
 
 var port = process.env.PORT || 5000;
